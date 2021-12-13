b6694e3350778ef334fce7445338fcb6a15f2b52
Zorbn <public.zorb@gmail.com>
2021-12-12 18:11:21 -0800

    Added emacs configuration

---
+++ .emacs.d/elpa/tree-sitter-langs-20211209.1639/tree-sitter-langs-pkg.el
@@ -0,0 +1,13 @@
+(define-package "tree-sitter-langs" "20211209.1639" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "d930b30d525b6418b9b40c3c2395e011a17e0749" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
